a781e8c256c3456f5701b0e85c960bd7cfbec7a6
Martinez <jamart41@student.uiwtx.edu>
2022-04-08 17:00:17 -0500

    started working on the pressure plates, currently WIP

---
+++ Source/Shinto/SM_PressurePlate.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/StaticMeshActor.h"
+#include "SM_PressurePlate.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHINTO_API ASM_PressurePlate : public AStaticMeshActor
+{
+	GENERATED_BODY()
+	
+
+
+public:
+	
+	UFUNCTION()
+	void OnOverlapBegin()
+	
+};
